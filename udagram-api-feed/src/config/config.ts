a23b360b8a2ce2d72012fba44d73cd38e8ce046b
nhutam123 <soledad.guitarist@gmail.com>
2023-06-24 08:03:08 +0700

    update

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -5,7 +5,10 @@ export const config = {
   host: "tamln2.cl970snglyp4.us-east-1.rds.amazonaws.com",
   dialect: "postgres",
   aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
+  aws_profile: {
+    AWS_SECRET_ACCESS_KEY: "Zah4fHXysQaG7YORGqD6RLcF3+R7/1dfwU7vXm3j",
+    AWS_ACCESS_KEY_ID: "AKIAS54FSHHQPKPC46MW",
+  },
   aws_media_bucket: "tamln2-udacity",
   url: process.env.URL,
   jwt: {
