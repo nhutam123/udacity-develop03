7cd5f86f35f3a390dc4f8fbd7bf6df62ac665a1c
nhutam123 <soledad.guitarist@gmail.com>
2023-07-02 04:13:52 +0700

    update replica

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -3,8 +3,8 @@ import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
 export const sequelize = new Sequelize({
-  'username': "postgres",
-  'password': "password",
+  'username': config.username,
+  'password': config.password,
   'database': config.database,
   'host': config.host,
 
