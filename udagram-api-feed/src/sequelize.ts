4f7bb888ddf7ef6f960bce9f57c80fd3867a11af
nhutam123 <soledad.guitarist@gmail.com>
2023-07-01 16:21:44 +0700

    udate

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -3,8 +3,8 @@ import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
 export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
+  'username': "postgres",
+  'password': "password",
   'database': config.database,
   'host': config.host,
 
